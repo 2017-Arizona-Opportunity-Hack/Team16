82c25f80a5625e7f8215f1f29d905ad7d930cb54
Zheng Xia <evanxia1018@gmail.com>
2017-10-22 13:58:58 -0700

    Final final comments

---
+++ Team16Solution/Team16Solution/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PreviousData";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NewData";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select Teachers.xlsx";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select Teachers.xlsx";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Click to Compute New People Ratio";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 277);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Team16Solution/Team16Solution/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OleDb;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace WindowsFormsApp1
+{
+
+    
+       public class Teachers
+        {
+            public String firstName { get; set; }
+            public String lastName { get; set; }
+            public String emailAddr { get; set; }
+            public String schoolName { get; set; }
+            public String schoolDist { get; set; }
+            public String city { get; set; }
+            public String county { get; set; }
+            public String gradeTaught { get; set; }
+            public String subjectTaught { get; set; }
+        }
+
+        public class WorkshopAttendence
+        {
+            public String year { get; set; }
+            public String date { get; set; }
+            public String workshopTitle { get; set; }
+            public String firstName { get; set; }
+            public String lastName { get; set; }
+        }
+
+        public partial class Form1 : Form
+    {
+        String file1Path = "";
+        String file2Path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "Output";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static List<WorkshopAttendence> getEvent(String filePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.DisplayAlerts = false;
+
+            Excel.Workbook teacher_book = excelApp.Workbooks.Open(Filename: filePath, ReadOnly: false);
+            Excel.Sheets Tables = teacher_book.Sheets;
+            Worksheet Table = Tables.get_Item("Info") as Worksheet;
+            // Excel._Worksheet Table = (Worksheet)teacher_book.Worksheets["Info"];
+
+            List<WorkshopAttendence> eventList = new List<WorkshopAttendence>();
+
+            int rows = Table.UsedRange.Row + Table.UsedRange.Rows.Count - 1;
+
+            Range range1 = (Range)Table.Cells[1, 2];
+            Range range2 = (Range)Table.Cells[rows, 5];
+            Range range = Table.get_Range(range1, range2);
+
+            object[,] values = (object[,])range.Value;
+
+            int i;
+            Range bColomn = Table.Cells[2, 1];
+            Range cColomn = Table.Cells[2, 2];
+            Range dColomn = Table.Cells[2, 3];
+            Range fColomn = Table.Cells[2, 4];
+            Range eColomn = Table.Cells[2, 5];
+
+            for (i = 2; i <= rows; i++)
+            {
+                var eventAtt = new WorkshopAttendence()
+                {
+                    year = (string)bColomn.get_Value(null),
+                    date = (string)cColomn.get_Value(null),
+                    firstName = (string)dColomn.get_Value(null),
+                    lastName = (string)fColomn.get_Value(null),
+                    workshopTitle = (string)eColomn.get_Value(null),
+
+                };
+                eventList.Add(eventAtt);
+            }
+
+            string data = string.Empty;
+
+            return eventList;
+
+        }
+
+
+        public static List<Teachers> getTeachers(String filePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.DisplayAlerts = false;
+
+            Excel.Workbook teacher_book = excelApp.Workbooks.Open(Filename: filePath, ReadOnly: false);
+            Excel.Sheets Tables = teacher_book.Sheets;
+            Worksheet Table = Tables.get_Item("Info") as Worksheet;
+            // Excel._Worksheet Table = (Worksheet)teacher_book.Worksheets["Info"];
+
+            List<Teachers> teacherList = new List<Teachers>();
+
+            /*
+            foreach (DataRow row in Table.Rows)
+            {
+                var teacher = new Teachers() {
+                    firstName = (row["First Name"].ToString())
+                    
+                };
+                Console.WriteLine(teacher.firstName);
+                teacherList.Add(teacher);
+            }
+            */
+
+
+            int rows = Table.UsedRange.Row + Table.UsedRange.Rows.Count - 1;
+
+            Range range1 = (Range)Table.Cells[1, 2];
+            Range range2 = (Range)Table.Cells[rows, 7];
+            Range range = Table.get_Range(range1, range2);
+
+            object[,] values = (object[,])range.Value;
+
+            int i;
+            Range bColomn = Table.Cells[2, 1];
+            Range cColomn = Table.Cells[2, 2];
+            Range dColomn = Table.Cells[2, 3];
+            Range fColomn = Table.Cells[2, 4];
+            Range eColomn = Table.Cells[2, 5];
+            Range gColomn = Table.Cells[2, 6];
+            Range iColomn = Table.Cells[2, 7];
+            Range jColomn = Table.Cells[2, 8];
+            Range kColomn = Table.Cells[2, 9];
+
+            for (i = 2; i <= rows; i++)
+            {
+                var teacher = new Teachers()
+                {
+                    firstName = (string)bColomn.get_Value(null),
+                    lastName = (string)cColomn.get_Value(null),
+                    emailAddr = (string)dColomn.get_Value(null),
+                    schoolName = (string)fColomn.get_Value(null),
+                    schoolDist = (string)eColomn.get_Value(null),
+                    city = (string)gColomn.get_Value(null),
+                    county = (string)iColomn.get_Value(null),
+                    gradeTaught = (string)jColomn.get_Value(null),
+                    subjectTaught = (string)kColomn.get_Value(null)
+
+                };
+                teacherList.Add(teacher);
+            }
+
+            string data = string.Empty;
+
+            Console.WriteLine();
+
+            return teacherList;
+
+        }
+        public static double calculateTuples(List<Teachers> oldList, List<Teachers> newList)
+        {
+            int index = 0;
+            int jndex = 0;
+            int newpeople = 0;
+            String previous = "";
+            bool flag = false;
+            double ratio = 0;
+
+
+            for (index = 0; index < newList.Count; index++)
+            {
+                for (jndex = 0; jndex < oldList.Count; jndex++)
+                {
+                    //go through the entire list and if this person isn't there this person is new
+                    if (((oldList[index].firstName.CompareTo(newList[jndex].firstName)) == 0) && ((oldList[index].lastName.CompareTo(newList[jndex].lastName)) == 0) && ((oldList[index].emailAddr.CompareTo(newList[jndex].emailAddr)) == 0))
+                    {
+                        flag = true;
+                        newpeople++;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+            }
+
+            ratio = (newpeople / newList.Count);
+
+
+            return ratio;
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //Get the Dialog-Information
+                label1.Text = ofd.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //Get the Dialog-Information
+                label2.Text = ofd.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<Teachers> value = getTeachers(label1.Text);
+            List<Teachers> value2 = getTeachers(label2.Text);
+            double output = calculateTuples(value, value2);
+            label3.Text = output.ToString();
+
+        }
+    }
+}
